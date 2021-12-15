8905d887433a8b260b21a542e0dd0c790038b6a2
marco <mazi2001@student.miun.se>
2021-12-15 18:06:41 +0100

    Move the featured and portfolio variables into the fetch requests.

---
+++ src/js/home.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// Variabler
+let id = 0;
+
+// GET-anrop
+// Hämtar webbplatser
+// Kontrollen förhindrar att fetch-anropet körs på portfoliosidan
+if (window.location.href.indexOf('portfolio.html') == -1) {
+    const featured = document.getElementById('featured');
+    fetch('https://studenter.miun.se/~mazi2001/writeable/dt173g/projekt/webbtjanst/api/api.php?cat=site')
+        // Konverterar svaret från JSON
+        .then(response => response.json())
+        // Loopar igenom och skriver ut
+        .then(data => data.forEach(element => {
+            // Begränsar till tre webbplatser
+            if (id <= 2) {
+                featured.innerHTML += 
+                    `<div class="featured">
+                        <h3 class="site-name">${element.site_name}</h3>
+                        <a class="find-out-more" href="website.html?id=${id}">
+                            <img src="${element.site_image_path}">
+                        </a>
+                    <div>`;  
+            }
+            id++;   
+        }
+    ));
+}
--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -1,12 +1,10 @@
 'use strict';
 
-// Variabler
-const portfolio = document.getElementById('portfolio');
-
 // GET-anrop
 // Hämtar webbplatser
 // Kontrollen förhindrar att fetch-anropet körs på startsidan
 if (window.location.href.indexOf('index.html') == -1) {
+    const portfolio = document.getElementById('portfolio');
     fetch('https://studenter.miun.se/~mazi2001/writeable/dt173g/projekt/webbtjanst/api/api.php?cat=site')
         // Konverterar svaret från JSON
         .then(response => response.json())
