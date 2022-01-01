5f63dbe23c47d795b9feb118014be8466babf6aa
marco <mazi2001@student.miun.se>
2022-01-01 22:51:02 +0100

    Move the error handling into a catch block.

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -8,24 +8,21 @@ if (window.location.href.indexOf('portfolio.html') !== -1) {
     fetch('https://studenter.miun.se/~mazi2001/writeable/dt173g/projekt/webbtjanst/api/api.php?cat=site')
         // Konverterar svaret från JSON
         .then(response => response.json())
-        .then(data => {
-            // Kontrollerar om svaret är en array (annars felmeddelande)
-            if (Array.isArray(data)) {
-                // Loopar igenom och skriver ut
-                data.forEach(element => {
-                    portfolio.innerHTML += 
-                        `<div class="site">
-                            <h3 class="site-name">${element.site_name}</h3>
-                            <a class="find-out-more" href="website.html?id=${id}">
-                                <img src="${element.site_image_path}">
-                            </a>
-                        <div>`;  
-                    id++;   
-                })
-            // Skriver ut felmeddelandet vid misslyckad databasanslutning
-            } else {
-                portfolio.innerHTML += `<p class="error">${data}</p>`;
-            }
-        }
-    );
+        // Loopar igenom och skriver ut
+        .then(data => data.forEach(element => {
+            portfolio.innerHTML += 
+                `<div class="site">
+                    <h3 class="site-name">${element.site_name}</h3>
+                    <a class="find-out-more" href="website.html?id=${id}">
+                        <img src="${element.site_image_path}">
+                    </a>
+                <div>`;  
+            id++;   
+        })
+    
+    )
+    // Skriver ut felmeddelandet vid misslyckad databasanslutning
+    .catch(error => {
+        portfolio.innerHTML += `<p class="error">${error}</p>`;
+    })
 }
