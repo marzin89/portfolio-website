36cd8ca33a82493ee98222e8aedb836111c9f23e
marco <mazi2001@student.miun.se>
2022-01-01 20:04:52 +0100

    Add error handling (db connection error) to the fetch-request.

--- src/js/website.js
+++ src/js/website.js
@@ -6,19 +6,25 @@ let siteID = url.searchParams.get('id');
 
 // Kontrollerar att det finns ett ID
 if (siteID) {
+    const title = document.querySelector('title');
+    const site = document.getElementById('site');
     // GET-anrop med ID
     fetch(`https://studenter.miun.se/~mazi2001/writeable/dt173g/projekt/webbtjanst/api/api.php?cat=site&id=${siteID}`)
     // Konverterar svaret från JSON
     .then(response => response.json())
     // Loopar igenom och skriver ut
     .then(data => {
-        const title = document.querySelector('title');
-        title.innerHTML = data.site_name;
-        const site = document.getElementById('site');
-        site.innerHTML +=   
-            `<h1>${data.site_name}</h1>
-            <img src="${data.site_image_path}">
-            <p>${data.site_description}</p>
-            <a href="${data.site_url}">Länk till webbplatsen</a>`
+        // Kontrollerar om site_name finns (annars felmeddelande)
+        if (data.site_name) {      
+            title.innerHTML = data.site_name;
+            site.innerHTML +=   
+                `<h1>${data.site_name}</h1>
+                <img src="${data.site_image_path}">
+                <p>${data.site_description}</p>
+                <a href="${data.site_url}">Länk till webbplatsen</a>`;
+        // Skriver ut felmeddelandet vid misslyckad databasanslutning
+        } else {
+            site.innerHTML += `<p class="error">${data}</p>`; 
+        }
     })
 }
